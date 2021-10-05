5aeb6446c021c5920080d2e221708de06d660b2c
insket <2694183181@qq.com>
2021-10-05 15:20:33 +0800

    '头部底部布局'

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 自己项目的充值样式和公共样式
+import '@/assets/styles/common.less'
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="container">
     <!-- 顶部 -->
-    <nav>top</nav>
+    <AppTopNav></AppTopNav>
     <!-- 头部 -->
-    <header>header</header>
+    <AppHader></AppHader>
     <!-- 内容容器 -->
     <div class="main">
       <router-view/>
     </div>
     <!-- 底部 -->
-    <footer>footer</footer>
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script>
+import AppTopNav from '@/components/app-topnav.vue'
+import AppHader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
   name: 'Layout',
+  components: {
+    AppTopNav,
+    AppHader,
+    AppFooter
+  },
   setup () {
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.main{
+  min-height: 600px;
+}
 </style>
